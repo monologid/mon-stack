6b038d598dedcf19c41f15e7ee585838329c5381
Faris <madebyais@gmail.com>
2023-02-26 06:25:18 +0700

    [mon-stack-frontend] Add new fetch-api for util and format code

---
+++ packages/mon-stack-frontend/src/utils/fetch-api.ts
@@ -0,0 +1,21 @@
+import querystring from 'querystring';
+
+export const FetchApi = async ({ url, method, headers, data }: any) => {
+	let opts: any = { method };
+
+	switch (method) {
+		case 'POST':
+			opts.body = JSON.stringify(data);
+			break;
+		default:
+			url = `url?${querystring.encode(data)}`;
+	}
+
+	opts.headers = {
+		...headers,
+		'Content-Type': 'application/json',
+	};
+
+	const response: any = await fetch(url, opts);
+	return await response.json();
+};
