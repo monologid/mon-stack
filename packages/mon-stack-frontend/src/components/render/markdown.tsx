c7c8c1c8da40a6007c13ccc16a8bdaab7e415eff
Faris <madebyais@gmail.com>
2023-03-23 18:00:13 +0700

    [mon-stack-frontend] Add component to render markdown

---
+++ packages/mon-stack-frontend/src/components/render/markdown.tsx
@@ -0,0 +1,7 @@
+import { remark } from "remark"
+import html from 'remark-html'
+
+export const RenderMarkdown = ({ text }: any) => {
+  const markdown = remark().use(html).processSync(text).toString()
+  return <div dangerouslySetInnerHTML={{ __html: markdown }} />
+}
