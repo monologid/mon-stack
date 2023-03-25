2a70967e0efb38a578ac3acafec94e58fdefce44
Faris <madebyais@gmail.com>
2023-03-25 10:16:06 +0700

    [mon-stack-frontend] Re-format code using prettier

--- packages/mon-stack-frontend/src/components/render/markdown.tsx
+++ packages/mon-stack-frontend/src/components/render/markdown.tsx
@@ -1,7 +1,7 @@
-import { remark } from "remark"
-import html from 'remark-html'
+import { remark } from 'remark';
+import html from 'remark-html';
 
 export const RenderMarkdown = ({ text }: any) => {
-  const markdown = remark().use(html).processSync(text).toString()
-  return <div dangerouslySetInnerHTML={{ __html: markdown }} />
-}
+  const markdown = remark().use(html).processSync(text).toString();
+  return <div dangerouslySetInnerHTML={{ __html: markdown }} />;
+};
--- packages/mon-stack-frontend/src/pages/index.tsx
+++ packages/mon-stack-frontend/src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home(props: any) {
     isTyping: false,
     user: 'MON',
     message: "Hi there, welcome! My name is MON, I'll be assisting you today. What can I help you? :)",
-    messages: []
+    messages: [],
   });
 
   const onInputKeyDown = async (e: any) => {
@@ -28,15 +28,36 @@ export default function Home(props: any) {
     e.target.value = '';
 
     setTimeout(async () => {
-      const url: string = '/api/v1/backend'
+      const url: string = '/api/v1/backend';
       const headers: any = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer demo'
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjc5NTgwNzYxLCJleHAiOjE2ODIxNzI3NjF9.rgg5WTTFQBPe2v0ad2HeyZTku25kCjphckX2vZxWNOg',
+      };
+      const response = await fetchApi({
+        url,
+        method: 'POST',
+        data: { method: 'POST', api: '/api/v1/cortex/talk', payload: { prompt } },
+        headers,
+      });
+      const { data } = response;
+      if (!data) return setState({ isTyping: false, message: 'Something went wrong. :(', messages: [] });
+
+      console.dir(data);
+
+      let newState: any = { isTyping: false };
+      switch (data.kind) {
+        case 'input':
+          newState = { ...newState, message: data.message, messages: [] };
+          break;
+        case 'error':
+          newState = { ...newState, message: data.message, messages: [] };
+          break;
+        default:
+          newState = { ...newState, messages: data.result };
       }
-      const response = await fetchApi({ url, method: 'POST', data: { method: 'POST', api: '/api/v1/cortex/talk', payload: { prompt } }, headers })
-      const { data } = response
-      if (data.kind == 'input') return setState({ isTyping: false, message: data.message, messages: [] });
-      setState({ isTyping: false, messages: data.result });
+
+      setState(newState);
     }, 1000);
   };
 
@@ -44,13 +65,19 @@ export default function Home(props: any) {
     setState({ isTyping: false, user: 'MON', message: 'Processing ...', messages: [] });
 
     setTimeout(async () => {
-      const url: string = '/api/v1/backend'
+      const url: string = '/api/v1/backend';
       const headers: any = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer demo'
-      }
-      const response = await fetchApi({ url, method: 'POST', data: { method: 'POST', api: '/api/v1/cortex/talk', payload: { prompt } }, headers })
-      const { data } = response
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjc5NTgwNzYxLCJleHAiOjE2ODIxNzI3NjF9.rgg5WTTFQBPe2v0ad2HeyZTku25kCjphckX2vZxWNOg',
+      };
+      const response = await fetchApi({
+        url,
+        method: 'POST',
+        data: { method: 'POST', api: '/api/v1/cortex/talk', payload: { prompt } },
+        headers,
+      });
+      const { data } = response;
       if (data.kind == 'input') return setState({ isTyping: false, message: data.message, messages: [] });
       setState({ isTyping: false, messages: data.result });
     }, 1000);
@@ -65,22 +92,23 @@ export default function Home(props: any) {
             className={'w-full bg-input-dark-primary rounded p-3 overflow-y-auto space-y-3'}
           >
             <div className={'font-bold text-xs tracking-wider'}>{state.user}</div>
-            {state.message && state.messages.length == 0 ?
+            {state.message && state.messages.length == 0 ? (
               <TypeAnimation
                 key={state.message}
                 cursor={true}
                 sequence={[state.message]}
                 speed={75}
                 className={'custom-cursor'}
-              /> : null
-            }
+              />
+            ) : null}
 
-            {state.messages.length > 1 ?
+            {state.messages.length > 0 ? (
               <>
-              {state.messages.map((item: any, i: number) => (
-                <RenderMarkdown text={item.message} />
-              ))}
-              </> : null}
+                {state.messages.map((item: any, i: number) => (
+                  <RenderMarkdown key={i} text={item.message} />
+                ))}
+              </>
+            ) : null}
           </div>
           <div className={'flex items-center space-x-3 bg-input-dark-primary rounded'}>
             <Input
